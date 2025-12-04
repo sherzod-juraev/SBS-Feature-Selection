97745fa695173b6e02c395e5cb2b93e0edfbdb73
JuraevSherzod <juraevsherzod9943@gmail.com>
2025-12-04 16:17:01 +0500

    router file updated in modules package

--- modules/router.py
+++ modules/router.py
@@ -1,5 +1,12 @@
 from fastapi import APIRouter, status
+from sklearn.linear_model import LogisticRegression
+from .scheme import SBSIn, SBSOut
+from SBS import SBS
 
+sbs = SBS(
+    LogisticRegression,
+    5
+)
 
 modules_router = APIRouter()
 
@@ -7,16 +14,14 @@ modules_router = APIRouter()
 @modules_router.post(
     '/',
     summary='SBS fit',
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
+    response_model=SBSOut
 )
-async def model_fit():
-    pass
-
-
-@modules_router.post(
-    '/predict',
-    summary='SBS predict',
-    status_code=status.HTTP_200_OK
-)
-async def model_predict():
-    pass
+async def model_fit(sbs_scheme: SBSIn) -> SBSOut:
+    indices = sbs.fit(sbs_scheme.X, sbs_scheme.y).indices
+    sbs_scheme = SBSOut(
+        X=sbs_scheme.X[:, indices].tolist(),
+        y=sbs_scheme.y.tolist(),
+        indices=indices.tolist()
+    )
+    return sbs_scheme
